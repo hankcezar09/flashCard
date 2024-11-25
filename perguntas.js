c88526fa09a16f2c6f90f30c08b4e0527a77edfb
hankcezar09 <146238345+hankcezar09@users.noreply.github.com>
2024-11-25 11:37:05 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -23,12 +23,12 @@ criaCartao(
     
     cartao.innerHTML = `
     <div class="cartao__conteudo">
-    <h3>${categoria}</h3>
+    <h3>{categoria}</h3>
     <div class="cartao__conteudo__pergunta">
-    <p>${pergunta}</p>
+    <p>{pergunta}</p>
     </div>
     <div class="cartao__conteudo__resposta">
-    <p>${resposta}</p>
+    <p>{resposta}</p>
     </div>
     </div>
     `
